d636a6dc0e2fd23962c9c21a6f27ecb2caa2652c
shanthshivam <shanthshivam@gmail.com>
2020-01-21 23:50:08 +0000

    Training HelloWorld

---
+++ HelloWorld/src/com/lloydsbanking/training/java/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * @author shant
+ */
+package com.lloydsbanking.training.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 0; i < 10; i++) {
+			if(i == 0)
+				System.out.println("Hello World!!! Printed the " + (i+1) + " time");
+			else
+				System.out.println("Hello World!!! Printed the " + (i+1) + " times");
+		}
+	}
+
+}
